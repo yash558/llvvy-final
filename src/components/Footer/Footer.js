242ec7706a36382f797d983fd3c596bc527fd65f
yash sharma <76558099+yash558@users.noreply.github.com>
2023-04-14 19:30:08 +0530

    final

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Footer.css'
 import logo from '../../assets/logo.png'
-import { Link } from 'react-scroll';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
